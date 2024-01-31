03e834b2af3882057bb6d539fe829cc0454d9004
Maicon Douglas <maiconboy11@gmail.com>
2024-01-31 12:09:50 -0300

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Home from "./page";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}
-      <Home></Home>
       </body>
     </html>
   );
