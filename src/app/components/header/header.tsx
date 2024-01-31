9e51af120987dbde28f74f57ba2e4cb9ec3e0019
Maicon Douglas <maiconboy11@gmail.com>
2024-01-31 12:19:14 -0300

    1

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -3,7 +3,9 @@ import "./header.scss"
 export default function Header(){
     return(
         <header>
-        
+            <nav>
+                <h1>bacana de mais</h1>
+            </nav>
         
         </header>
 
